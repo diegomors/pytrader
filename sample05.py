45cb1aeaba6317f87e6a336b7e1772fa152f28cc
Diego Moraes <diegomoraes.a@gmail.com>
2020-10-31 17:57:25 -0300

    simple and fastly candlestick graph

--- sample05.py
+++ sample05.py
@@ -1,14 +1,17 @@
-import plotly.graph_objects as go
+import mplfinance as fplt
 import pandas as pd
 
-columns = ['time', 'open', 'high', 'low', 'close', 'tick_volume', 'spread', 'real_volume']
-df = pd.read_csv('WINZ20M5.csv', names=columns)
-df['time']=pd.to_datetime(df['time'], format='%Y.%m.%d %H:%M' , errors='coerce')
+columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Ticks', 'Volume']
+only_columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
+df = pd.read_csv('WINZ20M5.csv', names=columns, usecols=only_columns)
+df['Date']=pd.to_datetime(df['Date'], format='%Y.%m.%d %H:%M' , errors='coerce')
+df = df.set_index('Date')
 
-fig = go.Figure(data=[go.Candlestick(x=df['time'],
-                open=df['open'],
-                high=df['high'],
-                low=df['low'],
-                close=df['close'])])
+df_subset = df.loc[(df.index >= '2020-10-30') & (df.index < '2020-10-31')]
 
-fig.show()
+fplt.plot(
+            df_subset,
+            type='candle',
+            title='WINZ20 M5',
+            ylabel='Preço (R$)'
+        )
